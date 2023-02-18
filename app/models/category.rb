c958b1dee8816ebffb1306aec51693de698b7f91
Shedrack-Sunday <hannania002@gmail.com>
2023-02-18 02:40:03 +0100

    Add models

---
+++ app/models/category.rb
@@ -0,0 +1,10 @@
+class Category < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image, dependent: :destroy
+  has_and_belongs_to_many :expenses, dependent: :destroy
+
+  # validations
+  validates :name, presence: true, uniqueness: { scope: :user_id }
+  validates :user_id, presence: true
+  validates_length_of :name, maximum: 200
+end
